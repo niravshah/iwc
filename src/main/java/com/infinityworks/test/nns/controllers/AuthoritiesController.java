f2b847e0b571ff279c27876c8e10ee00208f29ee
shahnirav <shah.nirav@me.com>
2016-11-08 08:47:37 +0000

    M

--- src/main/java/com/infinityworks/test/nns/controllers/AuthoritiesController.java
+++ src/main/java/com/infinityworks/test/nns/controllers/AuthoritiesController.java
@@ -2,9 +2,13 @@ package com.infinityworks.test.nns.controllers;
 
 import com.infinityworks.test.nns.domain.Authorities;
 import com.infinityworks.test.nns.services.AuthoritiesService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
 public class AuthoritiesController {
@@ -13,8 +17,8 @@ public class AuthoritiesController {
     private AuthoritiesService authoritiesService;
 
     @RequestMapping(value = "/api/authorities")
-    public Authorities getAuthorities() {
-        return authoritiesService.getAuthorities();
+    public ResponseEntity<Authorities> getAuthorities() {
+        return ok(authoritiesService.getAuthorities());
     }
 
 }
--- src/main/java/com/infinityworks/test/nns/controllers/EstablishmentsController.java
+++ src/main/java/com/infinityworks/test/nns/controllers/EstablishmentsController.java
@@ -3,9 +3,12 @@ package com.infinityworks.test.nns.controllers;
 import com.infinityworks.test.nns.domain.Establishments;
 import com.infinityworks.test.nns.services.EstablishmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
 public class EstablishmentsController {
@@ -14,8 +17,8 @@ public class EstablishmentsController {
     private EstablishmentService establishmentService;
 
     @RequestMapping(value = "/api/authority/{id}/establishments")
-    public Establishments getAuthorities(@PathVariable Integer id) {
-        return establishmentService.getEstablishmentsByLocalAuthorityId(id);
+    public ResponseEntity<Establishments> getAuthorities(@PathVariable Integer id) {
+        return ok(establishmentService.getEstablishmentsByLocalAuthorityId(id));
     }
 
 }
